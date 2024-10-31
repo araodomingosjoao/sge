f1b618ebed5b19814ded91f6379326bbfd18fd5a
Arao Domingos <araodomingos04@gmail.com>
2024-10-31 12:59:37 +0100

    Update Header.vue

--- frontend/src/components/Layout/Header.vue
+++ frontend/src/components/Layout/Header.vue
@@ -5,23 +5,23 @@
                 <div class="d-flex">
                     <!-- LOGO -->
                     <div class="navbar-brand-box horizontal-logo">
-                        <a href="index.html" class="logo logo-dark">
+                        <router-link to="/dashboard" class="logo logo-dark">
                             <span class="logo-sm">
                                 <img src="/assets_velzon/images/logo-sm.png" alt="" height="22">
                             </span>
                             <span class="logo-lg">
                                 <img src="/assets_velzon/images/logo-dark.png" alt="" height="17">
                             </span>
-                        </a>
+                        </router-link>
 
-                        <a href="index.html" class="logo logo-light">
+                        <router-link to="/dashboard" class="logo logo-light">
                             <span class="logo-sm">
                                 <img src="/assets_velzon/images/logo-sm.png" alt="" height="22">
                             </span>
                             <span class="logo-lg">
                                 <img src="/assets_velzon/images/logo-light.png" alt="" height="17">
                             </span>
-                        </a>
+                        </router-link>
                     </div>
 
                     <!-- Toggle Menu Button -->
