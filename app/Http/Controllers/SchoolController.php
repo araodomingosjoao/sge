02c9b3b5579fcf0fb9f22f0e793a8d3e9afcdfdb
Arao Domingos <araodomingos04@gmail.com>
2024-11-24 02:34:28 +0100

    Update SchoolController.php

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponse;
+use App\Http\Resources\School\SchoolResource;
 use App\Models\AcademicYear;
+use App\Models\Category;
 use App\Models\School;
 use App\Models\TypeEducation;
 use App\Repositories\SchoolRepository;
+use App\Traits\CrudTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -18,7 +21,30 @@ use Illuminate\Support\Facades\Log;
  */
 class SchoolController extends Controller
 {
-    protected $repository;
+    use CrudTrait;
+
+    protected $storeValidationRules = [];
+
+    protected $updateValidationRules = [
+        'type_education_id' => 'required|numeric|exists:type_educations,id',
+        'category_id' => 'required_if:type_education_id,4|string|exists:categories,id',
+        'school_name' => 'required|string|max:255',
+        'email' => 'nullable|email|unique:users,email',
+        'phone' => 'nullable|string|unique:users,phone|max:20',
+        'address' => 'nullable|string|max:255',
+        'city' => 'nullable|string|max:100',
+        'state' => 'nullable|string|max:100',
+        'country' => 'nullable|string|max:100',
+        'postal_code' => 'nullable|string|max:20',
+        'website' => 'nullable|url|max:255',
+        'founded_year' => 'nullable|numeric|min:1800',
+        'registration_number' => 'nullable|string|max:100',
+    ];
+
+    protected $uniqueFields = [];
+
+    protected $resource = SchoolResource::class;
+    protected $resourceDetails = SchoolResource::class;
 
     public function __construct(SchoolRepository $repository)
     {
@@ -42,6 +68,7 @@ class SchoolController extends Controller
     public function options()
     {
         return ApiResponse::success([
+            'optionsCategory' => Category::all(),
             'optionsTypeEducation' => TypeEducation::all()
         ]);
     }
